2c66169982b6fa4f9fa87befb56c8cfb45151a2c
vinzala <36253969+vinzala@users.noreply.github.com>
2018-05-27 19:50:08 -0400

    Update reality.py

--- reality.py
+++ reality.py
@@ -1,9 +1,9 @@
 class maintain:
-    ''' A class which will do nothing to alter the interpretation of keywords 'True' and 'False'. '''
+    ''' A class which will do nothing to alter the interpretation of 'True' and 'False'. '''
     def __init__(self):
         pass
 
 
-# If imported, the following assignments will invert the definition of keywords 'True' and 'False'.
+# If imported, the following assignments will invert the definition of 'True' and 'False'.
 True = False
 False = True
